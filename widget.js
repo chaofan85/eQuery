79a79c19ab87d11880be0ffe418a2dd49715241b
Chao Fan <fanchao05@gmail.com>
2017-11-09 16:11:55 -0500

    updated styling

--- widget.js
+++ widget.js
@@ -1,10 +1,11 @@
 $e(function() {
-  console.log($e('h2').html());
 
   $e('.submit').on('click', function(e){
     e.preventDefault();
     const value = document.querySelector('.input').value;
-    $e('.list').append(`<li>${value}</li>`);
+    if (value) {
+      $e('.list').append(`<li>${value}</li>`);
+    }
     document.querySelector('.input').value = "";
   });
 });
