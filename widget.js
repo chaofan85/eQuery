498a2075b62e29ac7c4ba18280e5841ae46c6bee
Chao Fan <fanchao05@gmail.com>
2017-11-10 09:17:50 -0500

    updated page

--- widget.js
+++ widget.js
@@ -8,4 +8,25 @@ $e(function() {
     }
     document.querySelector('.input').value = "";
   });
+
+  $e.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=391d793ba88597f366c30161211f6ee5',
+    dataType: 'json',
+    success(data) {
+
+      const jsonData = JSON.parse(data);
+      console.log(jsonData);
+      showWeatherInfo(jsonData);
+    },
+    error() {
+      console.error("Something's wrong.");
+    }
+  });
+
+  function showWeatherInfo(data) {
+    // debugger
+    console.log(data.weather[0].main);
+    console.log(data.main.temp, data.main.humidity);
+    return;
+  }
 });
